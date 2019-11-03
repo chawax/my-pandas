58d46af81eb5b7a6057dc07c3932679f7e804d89
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-03 22:03:45 +0100

    Utilisation de Axios pour charger la liste des pandas

--- src/containers/PandasListPage.tsx
+++ src/containers/PandasListPage.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import PandasList from '../components/PandasList';
-import pandas from '../pandas';
+import axios, { AxiosResponse } from 'axios';
+import { Panda } from '../types/Pandas';
 
 interface Props {}
 
-class PandasListPage extends React.Component<Props> {
+interface State {
+  pandas: Panda[];
+}
+
+class PandasListPage extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      pandas: [],
+    };
+  }
+
   handlePress = (key: string) => {
     alert(key);
   };
 
+  componentDidMount() {
+    axios.get('http://localhost:3004/pandas').then((response: AxiosResponse) => {
+      this.setState({
+        pandas: response.data,
+      });
+    });
+  }
+
   render() {
+    const { pandas } = this.state;
     return (
       <div style={{ padding: 20 }}>
         <PandasList pandas={pandas} onPress={this.handlePress} />
