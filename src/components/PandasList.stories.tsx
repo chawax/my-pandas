3adbb590fc4e8083b528487c1a875f06dc37eaee
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-04 15:52:15 +0100

    Format CSF

--- src/components/PandasList.stories.tsx
+++ src/components/PandasList.stories.tsx
@@ -1,11 +1,14 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 import pandas from '../pandas';
 import PandasList from './PandasList';
 
-storiesOf('PandasList', module).add('with pandas', () => (
+export default {
+  title: 'PandasList',
+};
+
+export const withPandas = () => (
   <div style={{ padding: 20 }}>
     <PandasList pandas={pandas} onPress={action('on press')} />
   </div>
-));
+);
