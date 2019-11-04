d3dbdc62beb07b4abfa878328ee090c7e0d50672
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-04 15:23:33 +0100

    Format CSF

--- src/components/PandaItem.stories.tsx
+++ src/components/PandaItem.stories.tsx
@@ -1,16 +1,25 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 import PandaItem from './PandaItem';
 
-storiesOf('PandaItem', module)
-  .add("sans centres d'intérêt", () => (
-    <div style={{ padding: 20 }}>
-      <PandaItem name="Nom du panda" onPress={action('on press')} />
-    </div>
-  ))
-  .add("avec centres d'intérêt", () => (
-    <div style={{ padding: 20 }}>
-      <PandaItem name="Nom du panda" interests={['cinéma', 'sport']} onPress={action('on press')} />
-    </div>
-  ));
+export default {
+  title: 'PandaItem',
+};
+
+export const sansCentresInteret = () => (
+  <div style={{ padding: 20 }}>
+    <PandaItem name="Nom du panda" onPress={action('on press')} />
+  </div>
+);
+sansCentresInteret.story = {
+  name: "Sans centres d'intérêt",
+};
+
+export const avecCentresInteret = () => (
+  <div style={{ padding: 20 }}>
+    <PandaItem name="Nom du panda" interests={['cinéma', 'sport']} onPress={action('on press')} />
+  </div>
+);
+avecCentresInteret.story = {
+  name: "Avec centres d'intérêt",
+};
