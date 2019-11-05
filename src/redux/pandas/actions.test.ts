4e17002b765bbc34f74ca7bd0790af3d039b495e
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-05 17:14:47 +0100

    Tests unitaires pour les action creators

--- src/redux/pandas/actions.test.ts
+++ src/redux/pandas/actions.test.ts
@@ -1,10 +1,26 @@
 import pandas from '../../pandas';
-import { loadPandasSuccess } from './actions';
+import { loadPandasFailure, loadPandasRequest, loadPandasSuccess } from './actions';
+
+test('loadPandasRequest', () => {
+  const action = loadPandasRequest();
+  expect(action).toEqual({
+    type: '@app/LOAD_PANDAS_REQUEST',
+  });
+});
 
 test('loadPandasSuccess', () => {
   const action = loadPandasSuccess(pandas);
   expect(action).toEqual({
     type: '@app/LOAD_PANDAS_SUCCESS',
     payload: pandas,
+  });
+});
+
+test('loadPandasFailure', () => {
+  const error = new Error('An error for test');
+  const action = loadPandasFailure(error);
+  expect(action).toEqual({
+    type: '@app/LOAD_PANDAS_FAILURE',
+    payload: error,
   });
 });
