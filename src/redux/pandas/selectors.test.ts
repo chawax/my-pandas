202f40b19d60281409cefab38aeb3aaa54b7b102
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-05 17:40:58 +0100

    Ajout de selectors

--- src/redux/pandas/selectors.test.ts
+++ src/redux/pandas/selectors.test.ts
@@ -1,6 +1,6 @@
 import pandas from '../../pandas';
+import { getError, getPandas, isFetching } from './selectors';
 import { AppState } from '../store';
-import { getPandas } from './selectors';
 
 test('getPandas', () => {
   const state: AppState = {
@@ -10,4 +10,26 @@ test('getPandas', () => {
     },
   };
   expect(getPandas(state)).toEqual(pandas);
+});
+
+test('isFetching', () => {
+  const state: AppState = {
+    pandas: {
+      data: [],
+      fetching: true,
+    },
+  };
+  expect(isFetching(state)).toBeTruthy();
+});
+
+test('getError', () => {
+  const error = new Error('an error for test');
+  const state: AppState = {
+    pandas: {
+      data: [],
+      fetching: false,
+      error,
+    },
+  };
+  expect(getError(state)).toEqual(error);
 });
--- src/redux/pandas/selectors.ts
+++ src/redux/pandas/selectors.ts
@@ -4,3 +4,11 @@ import { Panda } from '../../types/Pandas';
 export const getPandas = (state: AppState): Panda[] => {
   return state.pandas.data;
 };
+
+export const isFetching = (state: AppState): boolean => {
+  return state.pandas.fetching;
+};
+
+export const getError = (state: AppState): Error | undefined => {
+  return state.pandas.error;
+};
