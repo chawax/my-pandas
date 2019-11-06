e6e263562e02e756ca09fbd88d2a29194a9ad05e
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-06 14:35:32 +0100

    Configuration redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,18 +3,27 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagasMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 import { pandasReducer } from './pandas/reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   pandas: pandasReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export type AppState = ReturnType<typeof rootReducer>;
 
 const initialState = {};
 const sagaMiddleware = createSagasMiddleware();
 const middlewares: Middleware[] = [sagaMiddleware];
-const store: Store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
-
+const store: Store = createStore(persistedReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 
 export default store;
