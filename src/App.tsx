c02e3b3ecfd9b2abf9de1b40aa0d0cd2b0b4bf56
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-06 13:46:35 +0100

    Intégration du composant Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import PandaDetailsPage from './containers/PandaDetailsPage';
 import PandasListPage from './containers/PandasListPage';
 import store from './redux/store';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <PandasListPage />
+      <h2 className="text-danger" style={{ padding: 10 }}>
+        My pandas
+      </h2>
+      <Router>
+        <Route path="/" exact component={PandasListPage} />
+        <Route path="/pandas/:id" component={PandaDetailsPage} />
+      </Router>
     </Provider>
   );
 };
