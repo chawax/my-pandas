127e75dc765bcaa502c4fc54b8729d28836eca3a
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-10 18:36:33 +0100

    Ajout du reducer connected-react-router

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
+import { connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 import { applyMiddleware, combineReducers, createStore, Middleware, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer as formReducer } from 'redux-form';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagasMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 import { pandasReducer } from './pandas/reducers';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { reducer as formReducer } from 'redux-form';
 
 const persistConfig = {
   key: 'root',
@@ -13,9 +15,11 @@ const persistConfig = {
   blacklist: ['form'],
 };
 
+const history = createBrowserHistory();
 const rootReducer = combineReducers({
   pandas: pandasReducer,
   form: formReducer,
+  router: connectRouter(history),
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
