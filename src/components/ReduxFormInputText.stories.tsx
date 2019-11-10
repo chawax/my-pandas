d4da6a9d9eb063c7e6aff1db1e6e38957ec1f4b4
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-10 15:50:53 +0100

    Composant ReduxFormInputText

---
+++ src/components/ReduxFormInputText.stories.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { WrappedFieldInputProps, WrappedFieldMetaProps } from 'redux-form';
+import ReduxFormInputText from './ReduxFormInputText';
+
+export default {
+  title: 'ReduxFormInputText',
+};
+
+const defaultMeta: WrappedFieldMetaProps = {
+  active: true,
+  autofilled: false,
+  asyncValidating: false,
+  dirty: false,
+  dispatch: (): any => {},
+  error: undefined,
+  form: 'defaultForm',
+  initial: undefined,
+  invalid: false,
+  pristine: false,
+  submitting: false,
+  submitFailed: false,
+  touched: false,
+  valid: true,
+  visited: true,
+};
+
+const defaultInput: WrappedFieldInputProps = {
+  value: undefined,
+  name: 'name',
+  onBlur: () => {},
+  onChange: () => {},
+  onFocus: () => {},
+  onDragStart: () => {},
+  onDrop: () => {},
+};
+
+export const initial = () => {
+  const input: WrappedFieldInputProps = {
+    ...defaultInput,
+  };
+  const meta: WrappedFieldMetaProps = {
+    ...defaultMeta,
+  };
+  return (
+    <div style={{ padding: 20 }}>
+      <ReduxFormInputText
+        input={input}
+        name="name"
+        label="Nom"
+        placeholder="Saisissez votre nom"
+        required={true}
+        meta={meta}
+      />
+    </div>
+  );
+};
+
+export const valid = () => {
+  const input: WrappedFieldInputProps = {
+    ...defaultInput,
+    value: 'Une valeur',
+  };
+  const meta: WrappedFieldMetaProps = {
+    ...defaultMeta,
+    valid: true,
+    touched: true,
+  };
+  return (
+    <div style={{ padding: 20 }}>
+      <ReduxFormInputText
+        input={input}
+        name="name"
+        label="Nom"
+        placeholder="Saisissez votre nom"
+        required={true}
+        meta={meta}
+      />
+    </div>
+  );
+};
+
+export const invalid = () => {
+  const input: WrappedFieldInputProps = {
+    ...defaultInput,
+  };
+  const meta: WrappedFieldMetaProps = {
+    ...defaultMeta,
+    error: 'Valeur obligatoire',
+    valid: false,
+    touched: true,
+  };
+  return (
+    <div style={{ padding: 20 }}>
+      <ReduxFormInputText
+        input={input}
+        name="name"
+        label="Nom"
+        placeholder="Saisissez votre nom"
+        required={true}
+        meta={meta}
+      />
+    </div>
+  );
+};
---
+++ src/components/ReduxFormInputText.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FormGroup, Input, Label } from 'reactstrap';
+import { WrappedFieldInputProps, WrappedFieldMetaProps } from 'redux-form';
+
+interface Props {
+  input: WrappedFieldInputProps;
+  name: string;
+  label: string;
+  placeholder: string;
+  required: boolean;
+  meta: WrappedFieldMetaProps;
+}
+
+const ReduxFormInputText = (props: Props) => {
+  const {
+    input,
+    name,
+    label,
+    placeholder,
+    required,
+    meta: { error, touched },
+  } = props;
+
+  let className = '';
+  if (touched) {
+    className = error ? 'is-invalid' : 'is-valid';
+  }
+  return (
+    <FormGroup>
+      <Label for={name}>{label}</Label>
+      <Input
+        type="text"
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        required={required}
+        className={className}
+        {...input}
+      />
+      {touched && error && <div className="invalid-feedback">{error}</div>}
+    </FormGroup>
+  );
+};
+
+export default ReduxFormInputText;
