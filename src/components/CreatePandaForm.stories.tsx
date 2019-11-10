2c8598e6ced38a229f570816cd2ce15dde4dadb4
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-10 16:29:29 +0100

    Composant CreatePandaForm

---
+++ src/components/CreatePandaForm.stories.tsx
@@ -0,0 +1,35 @@
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, compose, createStore, Middleware } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import CreatePandaForm from './CreatePandaForm';
+
+export default {
+  title: 'CreatePandaForm',
+};
+
+const buildFormDecoratorStore = () => {
+  const appReducer = combineReducers({
+    form: formReducer,
+  });
+  const rootReducer = (state, action) => appReducer(state, action);
+  const middlewares: Middleware[] = [];
+  const store = createStore(rootReducer, {}, compose(applyMiddleware(...middlewares)));
+  return store;
+};
+
+const FormDecorator = props => {
+  const formDecoratorStore = buildFormDecoratorStore(props.initialState, props.reducers);
+  return <Provider store={formDecoratorStore}>{props.children}</Provider>;
+};
+
+export const creationForm = () => {
+  return (
+    <FormDecorator>
+      <div style={{ padding: 20 }}>
+        <CreatePandaForm onSubmit={action('on submit')} onCancel={action('on cancel')} />
+      </div>
+    </FormDecorator>
+  );
+};
---
+++ src/components/CreatePandaForm.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Button, Form } from 'reactstrap';
+import { Field, FormErrors, InjectedFormProps, reduxForm } from 'redux-form';
+import ReduxFormInput from './ReduxFormInput';
+
+interface OwnProps {
+  onCancel(): void;
+}
+
+interface FormData {
+  name: string;
+  interests: string;
+  image: string;
+}
+
+type Props = InjectedFormProps<FormData, OwnProps> & OwnProps;
+
+const CreatePandaForm: React.FC<Props> = (props: Props) => {
+  const { valid, onCancel, handleSubmit } = props;
+  return (
+    <Form noValidate onSubmit={handleSubmit}>
+      <Field
+        component={ReduxFormInput}
+        label="Nom"
+        type="text"
+        name="name"
+        placeholder="Saisissez le nom du panda"
+        required
+      />
+      <Field
+        component={ReduxFormInput}
+        label="Centres d'intérêt"
+        type="text"
+        name="interests"
+        placeholder="Saisissez les centres d'intérêts, séparés par des virgules"
+        required
+      />
+      <Field
+        component={ReduxFormInput}
+        label="Image"
+        type="text"
+        name="image"
+        placeholder="Saisissez l'url de l'image"
+        required
+      />
+      <Button color="primary" style={{ marginRight: 10 }} disabled={!valid}>
+        Valider
+      </Button>
+      <Button onClick={onCancel}>Annuler</Button>
+    </Form>
+  );
+};
+
+const validateForm = (values: FormData): FormErrors<FormData> => {
+  const errors: FormErrors<FormData> = {};
+  if (!values.name) {
+    errors.name = 'Valeur obligatoire';
+  }
+  if (!values.interests) {
+    errors.interests = 'Valeur obligatoire';
+  }
+  if (!values.image) {
+    errors.image = 'Valeur obligatoire';
+  }
+  return errors;
+};
+
+export default reduxForm<FormData, OwnProps>({
+  form: 'createPanda',
+  validate: validateForm,
+})(CreatePandaForm);
