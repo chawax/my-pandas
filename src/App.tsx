8743e617d58d78647d4c34b7860f8b8dc8e1df80
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-10 16:50:08 +0100

    Ajout d'une route /createPanda

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import CreatePandaPage from './containers/CreatePandaPage';
 import PandaDetailsPage from './containers/PandaDetailsPage';
 import PandasListPage from './containers/PandasListPage';
 import store from './redux/store';
@@ -15,6 +16,7 @@ const App: React.FC = () => {
       <Router>
         <Route path="/" exact component={PandasListPage} />
         <Route path="/pandas/:id" component={PandaDetailsPage} />
+        <Route path="/createPanda" component={CreatePandaPage} />
       </Router>
     </Provider>
   );
