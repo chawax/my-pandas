bb3d25edbd8899613180ddb25c03c4c243d92299
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-10 18:50:30 +0100

    Déclenchement saga createPanda

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,7 +1,10 @@
 import { all, takeLatest } from 'redux-saga/effects';
-import { loadPandas } from './pandas';
-import { LOAD_PANDAS_REQUEST } from '../redux/pandas/types';
+import { loadPandas, createPanda } from './pandas';
+import { LOAD_PANDAS_REQUEST, CREATE_PANDA_REQUEST } from '../redux/pandas/types';
 
 export default function* rootSaga() {
-  yield all([takeLatest(LOAD_PANDAS_REQUEST, loadPandas)]);
+  yield all([
+    takeLatest(LOAD_PANDAS_REQUEST, loadPandas), //
+    takeLatest(CREATE_PANDA_REQUEST, createPanda),
+  ]);
 }
