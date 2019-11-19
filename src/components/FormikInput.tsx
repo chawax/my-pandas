a3aff08ab5beb2357e5e808ef17d5714588022ef
Olivier THIERRY <othierry@MacBook-Pro-de-Olivier-THIERRY.local>
2019-11-19 16:09:02 +0100

    Champ input lié à Formik

---
+++ src/components/FormikInput.tsx
@@ -0,0 +1,33 @@
+import { FieldConfig, useField } from 'formik';
+import React from 'react';
+import { FormGroup, Input, Label } from 'reactstrap';
+
+interface OwnProps {
+  label: string;
+  placeholder: string;
+}
+
+type Props = FieldConfig & OwnProps;
+
+const FormikInput = (props: Props) => {
+  const [field, meta] = useField(props.name);
+  console.log(field);
+  console.log(meta);
+  const { label } = props;
+
+  let className = '';
+  if (meta.touched) {
+    className = meta.error ? 'is-invalid' : 'is-valid';
+  }
+
+  return (
+    <FormGroup>
+      <Label for={field.name}>{label}</Label>
+      //@ts-ignore
+      <Input {...field} {...props} className={className} />
+      <div className="invalid-feedback">{meta.error}</div>
+    </FormGroup>
+  );
+};
+
+export default FormikInput;
