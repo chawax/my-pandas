74f6f822f87a79a18a11eb6fa5b3c9253b8c9cea
Olivier THIERRY <chawax@users.noreply.github.com>
2020-01-15 21:09:04 +0100

    Upgrade Storybook 5.3 (#12)

--- .storybook/config.ts
+++
@@ -1,5 +0,0 @@
-import { configure } from '@storybook/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// automatically import all files ending in *.stories.tsx
-configure(require.context('../src', true, /(\.)?stories\.(tsx|jsx|js)?$/), module);
