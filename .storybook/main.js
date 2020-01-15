9b4e70955507d1849f9abbc3ae083c65492d3915
Olivier Thierry <olivier.thierry@gmail.com>
2020-01-15 21:06:06 +0100

    Upgrade Storybook 5.3

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(js|tsx)', '../src/**/stories.(js|tsx)'],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links/register'],
+};
