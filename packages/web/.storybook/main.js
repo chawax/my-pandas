9aedeb02138cf2890be4e044a3ac224555dfb7ed
Olivier Thierry <olivier.thierry@gmail.com>
2020-02-29 20:22:38 +0100

    Ajout du addon a11y à Storybook

--- packages/web/.storybook/main.js
+++ packages/web/.storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|tsx)', '../src/**/stories.(js|tsx)'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links/register'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-links/register',
+  ],
 };
