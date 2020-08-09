862e566c5ede1a94022bcf0a5b15f3dca6d703d5
Olivier Thierry <olivier.thierry@gmail.com>
2020-08-09 16:12:19 +0200

    Remplacement de Sinon par les mocks Jest

--- src/services/api.test.ts
+++ src/services/api.test.ts
@@ -1,24 +1,24 @@
 import { fail } from 'assert';
 import axios from 'axios';
-import sinon, { SinonSandbox } from 'sinon';
 import api from './api';
 
 describe('loadPandas', () => {
-  let sandbox: SinonSandbox;
-  beforeEach(() => (sandbox = sinon.createSandbox()));
-  afterEach(() => sandbox.restore());
+  let mockGetAxios: jest.SpyInstance<any>;
+  beforeEach(() => {
+    mockGetAxios = jest.spyOn(axios, 'get');
+  });
 
   test('success', async () => {
     const pandas = ['panda1', 'panda2', 'panda3'];
     const resolved = { data: pandas };
-    sandbox.stub(axios, 'get').resolves(resolved);
+    mockGetAxios.mockResolvedValueOnce(resolved);
     const result = await api.loadPandas();
     expect(result).toEqual(pandas);
   });
 
   test('failure', async () => {
     const rejected = { error: { response: { status: 404 } } };
-    sandbox.stub(axios, 'get').rejects(rejected);
+    mockGetAxios.mockRejectedValueOnce(rejected);
     try {
       await api.loadPandas();
       fail();
