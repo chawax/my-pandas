17e9cbf06c6bb5faaa296f88f4de1b7020cc0504
Olivier Thierry <olivier.thierry@gmail.com>
2020-08-18 20:09:15 +0200

    Uniquement le plugin actions

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y/register',
-    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/preset-create-react-app',
   ],
