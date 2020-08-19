d3fc8c3afb8f4356af57225305be3a5a21439562
Olivier Thierry <olivier.thierry@gmail.com>
2020-08-19 10:19:23 +0200

    Installation du addon essentials avec désactivation de controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,12 @@ module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y/register',
-    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        controls: false,
+      },
+    },
     '@storybook/addon-links',
     '@storybook/preset-create-react-app',
   ],
