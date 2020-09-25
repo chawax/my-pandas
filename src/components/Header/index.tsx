904c9bae63c9d189bf9a8ee2753857a8f3f620eb
Olivier THIERRY <othierry@asi.fr>
2020-09-25 08:30:50 +0200

    Composant Title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, UncontrolledDropdown } from 'reactstrap';
+import styled from 'styled-components';
 
 const languages = ['en', 'fr'];
 
+interface TitleProps {
+  primary?: boolean;
+}
+
+const Title = styled.h1<TitleProps>`
+  font-size: 1.5em;
+  text-align: center;
+  color: ${(props) => (props.primary ? 'blue' : 'red')};
+`;
+
 const Header = () => {
   const { t, i18n } = useTranslation();
+  const language = i18n.language;
   return (
     <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/">{t('app.title')}</NavbarBrand>
+      <NavbarBrand href="/">
+        <Title primary={language === 'fr'}>{t('app.title')}</Title>
+      </NavbarBrand>
       <Nav className="mr-auto" navbar>
         <UncontrolledDropdown nav inNavbar>
           <DropdownToggle nav caret>
             Langue
           </DropdownToggle>
           <DropdownMenu right>
-            {languages.map(language => (
+            {languages.map((language) => (
               <DropdownItem
                 key={language}
                 onClick={() => {
