5ca1cc8af2a2611c0750b0eb4e5d442844cba6ef
Olivier THIERRY <chawax@users.noreply.github.com>
2020-09-28 22:44:48 +0200

    React Testing library (#31)
    * Exemple de React Testing Library
    
    * Utilisation de jest.mock
    
    * Tests du composant PandaItem
    
    * Mise à jour du README

---
+++ src/components/PandaItem/PandaItem.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+import PandaItem from '.';
+
+afterEach(cleanup);
+
+it('renders panda name', () => {
+  render(<PandaItem name="Panda 1" onPress={jest.fn()} />);
+  expect(screen.getByText('Panda 1')).toBeInTheDocument();
+});
+
+it('onPress event is fired', () => {
+  const handlePress = jest.fn();
+  render(<PandaItem name="Panda 1" onPress={handlePress} />);
+  //screen.debug();
+  //screen.getByRole('');
+  fireEvent.click(screen.getByRole('listitem'));
+  expect(handlePress).toHaveBeenCalledTimes(1);
+});
--- src/services/api.test.ts
+++ src/services/api.test.ts
@@ -2,23 +2,21 @@ import { fail } from 'assert';
 import axios from 'axios';
 import api from './api';
 
-describe('loadPandas', () => {
-  let mockGetAxios: jest.SpyInstance<any>;
-  beforeEach(() => {
-    mockGetAxios = jest.spyOn(axios, 'get');
-  });
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+describe('loadPandas', () => {
   test('success', async () => {
     const pandas = ['panda1', 'panda2', 'panda3'];
     const resolved = { data: pandas };
-    mockGetAxios.mockResolvedValueOnce(resolved);
+    mockedAxios.get.mockResolvedValue(resolved);
     const result = await api.loadPandas();
     expect(result).toEqual(pandas);
   });
 
   test('failure', async () => {
     const rejected = { error: { response: { status: 404 } } };
-    mockGetAxios.mockRejectedValueOnce(rejected);
+    mockedAxios.get.mockRejectedValue(rejected);
     try {
       await api.loadPandas();
       fail();
