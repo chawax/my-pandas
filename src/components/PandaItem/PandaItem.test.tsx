00072b42bf2bc24ce85b3bcfa665d40fc5c997ba
Olivier Thierry <olivier.thierry@gmail.com>
2020-09-28 22:36:05 +0200

    Tests du composant PandaItem

--- src/components/PandaItem/PandaItem.test.tsx
+++ src/components/PandaItem/PandaItem.test.tsx
@@ -4,6 +4,11 @@ import PandaItem from '.';
 
 afterEach(cleanup);
 
+it('renders panda name', () => {
+  render(<PandaItem name="Panda 1" onPress={jest.fn()} />);
+  expect(screen.getByText('Panda 1')).toBeInTheDocument();
+});
+
 it('onPress event is fired', () => {
   const handlePress = jest.fn();
   render(<PandaItem name="Panda 1" onPress={handlePress} />);
