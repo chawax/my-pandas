01e14a496db8ba007521030e751b334e95728bcf
Olivier Thierry <olivier.thierry@gmail.com>
2020-11-03 23:15:16 +0100

    Utilisation de userEvent

--- src/components/PandaItem/PandaItem.test.tsx
+++ src/components/PandaItem/PandaItem.test.tsx
@@ -1,12 +1,17 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { render, fireEvent, cleanup, screen } from '@testing-library/react';
 import PandaItem from './PandaItem';
 
 afterEach(cleanup);
 
-it('renders panda name', () => {
-  render(<PandaItem name="Panda 1" onPress={jest.fn()} />);
+it('renders panda infos', () => {
+  const interests = ['dormir', 'manger du bambou'];
+  render(<PandaItem name="Panda 1" interests={interests} onPress={jest.fn()} />);
+  //screen.debug();
   expect(screen.getByText('Panda 1')).toBeInTheDocument();
+  expect(screen.getByText('dormir')).toBeInTheDocument();
+  expect(screen.getByText('manger du bambou')).toBeInTheDocument();
 });
 
 it('onPress event is fired', () => {
@@ -14,6 +19,6 @@ it('onPress event is fired', () => {
   render(<PandaItem name="Panda 1" onPress={handlePress} />);
   //screen.debug();
   //screen.getByRole('');
-  fireEvent.click(screen.getByRole('listitem'));
+  userEvent.click(screen.getByRole('listitem'));
   expect(handlePress).toHaveBeenCalledTimes(1);
 });
